27db1358c903ab1247449024652a43a7546d9c15
Casey Cassler <casey.cassler@eucalyptus.com>
2015-05-12 14:45:35 -0700

    Repos validation update

--- eucadeploy/plugins/validator/repos.py
+++ eucadeploy/plugins/validator/repos.py
@@ -4,20 +4,27 @@ from urllib2 import Request, urlopen, URLError
 
 class Repos(ValidatorPlugin):
     def validate(self):
-        self.repos = [self.environment['default_attributes']['eucalyptus']['enterprise-repo']]
-        self.repos += [self.environment['default_attributes']['eucalyptus']['default-img-url']]
-        self.repos += [self.environment['default_attributes']['eucalyptus']['euca2ools-repo']]
-        self.repos += [self.environment['default_attributes']['eucalyptus']['eucalyptus-repo']]
-        self.repos += [self.environment['default_attributes']['eucalyptus']['init-script-url']]
+        eucalyptus_attributes = self.environment['default_attributes']['eucalyptus']
+        self.repos = []
+        if 'enterprise-repo' in eucalyptus_attributes:
+            self.repos.append(eucalyptus_attributes['enterprise-repo'])
+        if 'default-img-url' in eucalyptus_attributes:
+            self.repos.append(eucalyptus_attributes['default-img-url'])
+        if 'euca2ools-repo' in eucalyptus_attributes:
+            self.repos.append(eucalyptus_attributes['euca2ools-repo'])
+        if 'eucalyptus-repo' in eucalyptus_attributes:
+            self.repos.append(eucalyptus_attributes['eucalyptus-repo'])
+        if 'init-script-url' in eucalyptus_attributes:
+            self.repos.append(eucalyptus_attributes['init-script-url'])
+        if 'post-script-url' in eucalyptus_attributes:
+            self.repos.append(eucalyptus_attributes['post-script-url'])
         for url in self.repos:
             req = Request(url)
             try:
                 response = urlopen(req)
+                self.success('URL: ' + str(url) + ' is valid and reachable!')
             except URLError, e:
                 if hasattr(e, 'reason'):
                     raise AssertionError("INVALID URL: " + str(url) + "  " + str(e.reason))
                 elif hasattr(e, 'code'):
-                    raise AssertionError("INVALID REQUEST: " + url + "  " + str(e.reason))
-            else:
-                # everything is fine
-                self.success('URL: ' + url + ' is valid and reachable!')
+                    raise AssertionError("INVALID REQUEST: " + str(url) + "  " + str(e.reason))
