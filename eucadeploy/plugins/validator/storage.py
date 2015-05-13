e734101d7e040abc98e1a1df2ae0f0752e88c011
Casey Cassler <casey.cassler@eucalyptus.com>
2015-05-13 01:04:31 -0700

    storage validation modifications

--- eucadeploy/plugins/validator/storage.py
+++ eucadeploy/plugins/validator/storage.py
@@ -7,7 +7,17 @@ class Storage(ValidatorPlugin):
         for name in self.topology['clusters'].keys():
             if 'storage-backend' in self.topology['clusters'][name]:
                 if 'netapp' in self.topology['clusters'][name]['storage-backend']:
+                    assert self.systemproperties[name + '.storage.chapuser']
+                    self.success('Netapp system property ' + name + '.storage.chapuser is valid.')
+                    assert self.systemproperties[name + '.storage.ncpaths']
+                    self.success('Netapp system property ' + name + '.storage.ncpaths is valid.')
                     assert self.systemproperties[name + '.storage.scpaths']
                     self.success('Netapp system property ' + name + '.storage.scpaths is valid.')
-                    #raise AssertionError("Netapp system property  " + name + " NOT FOUND")
-
+                    assert self.systemproperties[name + '.storage.sanhost']
+                    self.success('Netapp system property ' + name + '.storage.sanhost is valid.')
+                    assert self.systemproperties[name + '.storage.sanpassword']
+                    self.success('Netapp system property ' + name + '.storage.sanpassword is valid.')
+                    assert self.systemproperties[name + '.storage.sanuser']
+                    self.success('Netapp system property ' + name + '.storage.sanuser is valid.')
+                    assert self.systemproperties[name + '.storage.vservername']
+                    self.success('Netapp system property ' + name + '.storage.vservername is valid.')
