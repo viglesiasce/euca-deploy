ae63d4ffeb63c10305b50ece50db5a3dcbee0254
Harold Spencer Jr <hspencer77@gmail.com>
2015-05-18 17:33:43 -0400

    Fix single line comments in debuggerplugin.py

--- eucadeploy/plugins/debugger/debuggerplugin.py
+++ eucadeploy/plugins/debugger/debuggerplugin.py
@@ -11,8 +11,7 @@ import six
 
 @six.add_metaclass(abc.ABCMeta)
 class DebuggerPlugin(object):
-    """Base class for example plugin used in the tutorial.
-    """
+    #Base class for example plugin used in the tutorial.
 
     def __init__(self, component_deployer):
         self.passed = 0
@@ -29,36 +28,26 @@ class DebuggerPlugin(object):
         self.report()
 
     def success(self, message):
-        """
-        Function to display and tally success of a debug step
-        """
+        # Function to display and tally success of a debug step
         self.passed +=1
         print green(self.message_style.format('DEBUG PASSED', message))
 
     def failure(self, message):
-        """
-        Function to display and tally a failure of a debug step
-        """
+        # Function to display and tally a failure of a debug step
         self.failed +=1
         print red(self.message_style.format('DEBUG FAILED', message))
 
     def info(self, message):
-        """
-        Function to display information of a debug step
-        """
+        # Function to display information of a debug step
         print white(self.message_style.format('INFO', message))
 
     def warning(self, message):
-        """
-        Function to display and tally a warning of a debug step
-        """
+        # Function to display and tally a warning of a debug step
         self.warnings +=1
         print yellow(self.message_style.format('DEBUG WARNING', message))
 
     def report(self):
-        """
-        Function to display report of debug results
-        """
+        # Function to display report of debug results
         text_color = red
         if self.failed == 0:
             text_color = cyan
@@ -71,9 +60,7 @@ class DebuggerPlugin(object):
 
     @task
     def run_command_task(command, user='root', password='foobar'):
-        """
-        Task to run command on host and return result
-        """
+        # Task to run command on host and return result
         env.user = user
         env.password = password
         env.parallel = True
@@ -81,9 +68,7 @@ class DebuggerPlugin(object):
 
     @task
     def get_file_task(remote_path, local_path, user='root', password='foobar'):
-        """
-        Task to grab file from host and return result
-        """
+        # Task to grab file from host and return result
         env.user = user
         env.password = password
         env.parallel = True
@@ -91,22 +76,16 @@ class DebuggerPlugin(object):
             return get(remote_path, local_path)
 
     def get_file_on_host(self, remote_path, local_path, host):
-        """
-        Function to execute get_file_task on host
-        """
+        # Function to execute get_file_task on host
         return execute(self.get_file_task, remote_path=remote_path,
                        local_path=local_path, host=host)[host]
 
     def run_command_on_hosts(self, command, hosts, host=None):
-        """
-        Function to execute run_command_task on list of hosts
-        """
+        # Function to execute run_command_task on list of hosts
         return execute(self.run_command_task, command=command, hosts=hosts)
 
     def run_command_on_host(self, command, host):
-        """
-        Function to execute run_command_task on host
-        """
+        # Function to execute run_command_task on host
         return execute(self.run_command_task, command=command, host=host)[host]
 
     def debug(self):
